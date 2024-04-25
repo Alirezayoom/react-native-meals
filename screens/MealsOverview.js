b53ada0ca6cd8a2f6bd514727b13d6feee818bbb
alirezayoom <alirezayoom@gmail.com>
2024-04-25 15:55:52 +0430

    add dynamic navigation options

--- screens/MealsOverview.js
+++ screens/MealsOverview.js
@@ -1,9 +1,17 @@
-import { FlatList, Text } from "react-native";
-import { MEALS } from "../data/dummy-data";
+import { FlatList } from "react-native";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 import MealItem from "../Components/MealItem";
+import { useLayoutEffect } from "react";
 
-function MealsOverview({ route }) {
+function MealsOverview({ route, navigation }) {
   const { categoryId } = route.params;
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === categoryId
+    ).title;
+    navigation.setOptions({ title: categoryTitle });
+  }, [categoryId, navigation]);
 
   const displayMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(categoryId) >= 0;
