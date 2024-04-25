a82808bc2d1dea76e7a06aabef45bebe99745750
alirezayoom <alirezayoom@gmail.com>
2024-04-25 13:03:43 +0430

    display meals of each category

--- screens/MealsOverview.js
+++ screens/MealsOverview.js
@@ -1,8 +1,25 @@
-import { Text } from "react-native";
+import { FlatList, Text } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../Components/MealItem";
 
 function MealsOverview({ route }) {
   const { categoryId } = route.params;
-  return <Text>MealsOverview - {categoryId}</Text>;
+
+  const displayMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(categoryId) >= 0;
+  });
+
+  const renderMealItems = (itemData) => {
+    return <MealItem title={itemData.item.title} />;
+  };
+
+  return (
+    <FlatList
+      data={displayMeals}
+      keyExtractor={(item) => item.id}
+      renderItem={renderMealItems}
+    />
+  );
 }
 
 export default MealsOverview;
