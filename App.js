713b5f5bf7c932eb25caf6e356d0c9a9a7a82e01
alirezayoom <alirezayoom@gmail.com>
2024-04-25 10:27:29 +0430

    add reactnavigation pkg

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CategoriesScreen from "./screens/CategoriesScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar barStyle={"light-content"} />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
